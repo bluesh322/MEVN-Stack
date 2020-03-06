aa784898e1d7876ef9efd2e9adce38c64ffd506d
bluesh322 <bluesh322@gmail.com>
2020-03-06 10:47:05 -0600

    Initial backend created

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require("./routes/api/posts");
+
+const port = process.env.PORT || 5000;
+
+app.use('/api/posts', posts);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
---
+++ server/routes/api/posts.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+
+
+//GET posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+//ADD posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createAt: new Date()
+    });
+    res.status(201).send();
+});
+
+//Delete posts
+router.delete('/:id', async (req, res) =>{
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({
+    _id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://root:root@cluster0-jqcld.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+
+    return client.db('Cluster0').collection('posts');
+}
+
+module.exports = router;
